c08142c78e62958eb2cea5786876a6c6562c8eeb
vishal02011 <vishalmaurya.vm23@gmail.com>
2026-06-02 22:05:39 +0530

    successfully added api and fetched data

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,40 @@ import { Left } from './components/Left'
 import { Rightbottom } from './components/Rightbottom'
 import { Search } from './components/Search'
 import { Tempgraph } from './components/Tempgraph'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
 
 const App = () => {
+  const API_KEY = '9c11a5305e2c78e570fe31a352f06f74'
+  const [searchTerm, setSearchTerm] = useState('')
+  const [weatherData, setWeatherData] = useState(null)
+  const [city, setCity]=useState("delhi")
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ API_KEY }&units=metric`
+  const [loading, setLoading]=useState(true)
+  const [error, setError]=useState(null)
+ 
+  useEffect(() => {
+    console.log("use effect called")
+    setError(null)
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      setWeatherData(data)
+      console.log(data)
+      setLoading(false)
+    })
+    .catch((err) => {
+      setError("No result Found.Try something else.")
+      console.log("not found")
+    })
+  }, [city])
+
   return (
     <div className='rot'>
       <Left/>
-      <Search/>
-      <Citycard/>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} setCity={setCity}/>
+      {loading ? (<p>Loading...</p>) : error ? ( <p>{error}</p>) : ( <Citycard data={weatherData}/>)}
       <HourForecast/>
       <DayForecast/>
       <Tempgraph/>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import search from "../assets/search.svg";
 
-export const Search = () => {
+export const Search = ({ searchTerm, setSearchTerm, setCity }) => {
   return (
     <div className='search'>
-      <input type="text" placeholder="Search for a city..." />
+      <button onClick={()=>{if (length.searchTerm!=0){setCity(searchTerm);console.log("done",searchTerm)}}}><img src={search} alt="Search" /></button>
+      <input type="text" placeholder="Search for a city..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
     </div>
   )
 }
